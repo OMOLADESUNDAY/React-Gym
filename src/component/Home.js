3b4dd5957e1f012785d2fb977a229bd96c38f724
omolade sunday <sundayomoladee11@gmail.com>
2023-01-10 14:06:51 +0100

    second commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import downloadImage from '../image/gym-girl4.jpg'
-import downloadImage2 from "../image/gym-guy5.jpg";
-import { featuresData,testimoniesData } from '../data/data'
-import {IoIosArrowDroprightCircle,IoIosArrowDropleftCircle} from "react-icons/io";
+// import { Link } from 'react-router-dom'
+// import downloadImage from '../image/gym-girl4.jpg'
+// import downloadImage2 from "../image/gym-guy5.jpg";
+// import { featuresData,testimoniesData } from '../data/data'
+// import {IoIosArrowDroprightCircle,IoIosArrowDropleftCircle} from "react-icons/io";
 import './home.css'
 
-const Slider=(direction)=>{
-return(
-    console.log('clicked '+ direction)
-  )
-}
+// const Slider=(direction)=>{
+// return(
+//     console.log('clicked '+ direction)
+//   )
+// }
 
 
 const Home = () => {
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { BiMenu } from "react-icons/bi";
-import { TiTimes } from "react-icons/ti";
-import { useRef } from "react";
+// import { Link } from "react-router-dom";
+// import { BiMenu } from "react-icons/bi";
+// import { TiTimes } from "react-icons/ti";
+// import { useRef } from "react";
 import "./navbar.css";
 const Navbar = () => {
 
